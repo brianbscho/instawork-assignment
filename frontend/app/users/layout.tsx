94243d6892739894e20c8767edf9e770267ebf34
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-11-17 15:37:31 +0900

    Render UserList component from users layout

--- frontend/app/users/layout.tsx
+++ frontend/app/users/layout.tsx
@@ -1,8 +1,10 @@
+import UserList from "@/components/UserList";
 import React from "react";
 
 const UsersLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full h-full py-12 px-7 overflow-auto relative">
+      <UserList />
       {children}
     </div>
   );
