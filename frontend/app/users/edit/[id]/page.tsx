52c7f121753fee9ff1eefad8cd38f099bc704ff7
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-11-18 10:20:15 +0900

    Add default data to edit page
    - use [id] param to retrieve user data
    - show user data into the fields in the form
    - indicate loading state

--- frontend/app/users/edit/[id]/page.tsx
+++ frontend/app/users/edit/[id]/page.tsx
@@ -1,17 +1,53 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { X } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import UserForm from "@/components/UserForm";
-import { RoleType } from "@/util/type";
+import { RoleType, UserType } from "@/util/type";
+import useUserStore from "@/store/user";
 
-const Edit = () => {
+const Edit = ({ params }: { params: Promise<{ id: string }> }) => {
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
   const [phonenumber, setPhonenumber] = useState("");
   const [role, setRole] = useState<RoleType>("REG");
+
+  const users = useUserStore((state) => state.users);
+  const [user, setUser] = useState<UserType>();
+  useEffect(() => {
+    const getUser = async () => {
+      if (!users) {
+        return;
+      }
+
+      const id = (await params).id;
+      const index = users.findIndex((u) => u.id === parseInt(id));
+      if (index < 0) {
+        alert("Invalid user id!");
+        router.push("/users");
+        return;
+      }
+
+      setUser(users[index]);
+    };
+    getUser();
+  }, [users, params]);
+
+  const router = useRouter();
+  const [isSettingUser, setIsSettingUser] = useState(true);
+  useEffect(() => {
+    if (!user) return;
+
+    setFirstname(user.firstname);
+    setLastname(user.lastname);
+    setEmail(user.email);
+    setPhonenumber(user.phonenumber);
+    setRole(user.role);
+    setIsSettingUser(false);
+  }, [user]);
 
   return (
     <div className="absolute w-full h-full inset-0 overflow-y-auto py-12 px-7 bg-white">
@@ -24,29 +60,35 @@ const Edit = () => {
       <div className="text-gray-400 mb-12">
         Edit contact info, location, and role.
       </div>
-      <div className="border-t py-3">
-        <form>
-          <UserForm
-            useFirstname={[firstname, setFirstname]}
-            useLastname={[lastname, setLastname]}
-            useEmail={[email, setEmail]}
-            usePhonenumber={[phonenumber, setPhonenumber]}
-            useRole={[role, setRole]}
-          />
-          <div className="flex justify-between mt-12">
-            <button>
-              <div className="px-7 py-3 text-red-500 rounded-md border border-gray-300">
-                Delete
-              </div>
-            </button>
-            <button type="submit">
-              <div className="px-7 py-3 bg-blue-500 text-white rounded-md">
-                Save
-              </div>
-            </button>
-          </div>
-        </form>
-      </div>
+      {!users ? (
+        <div className="py-12 text-center text-lg">Loading...</div>
+      ) : !user || isSettingUser ? (
+        <></>
+      ) : (
+        <div className="border-t py-3">
+          <form>
+            <UserForm
+              useFirstname={[firstname, setFirstname]}
+              useLastname={[lastname, setLastname]}
+              useEmail={[email, setEmail]}
+              usePhonenumber={[phonenumber, setPhonenumber]}
+              useRole={[role, setRole]}
+            />
+            <div className="flex justify-between mt-12">
+              <button>
+                <div className="px-7 py-3 text-red-500 rounded-md border border-gray-300">
+                  Delete
+                </div>
+              </button>
+              <button type="submit">
+                <div className="px-7 py-3 bg-blue-500 text-white rounded-md">
+                  Save
+                </div>
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
