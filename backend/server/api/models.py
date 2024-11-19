e74953e3fb8ab197a3ab4726eb5da7289151bf28
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2024-11-19 09:54:06 +0900

    Add email and phone number validator to User model in backend

--- backend/server/api/models.py
+++ backend/server/api/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.core.validators import EmailValidator, RegexValidator
+import re
+
+class PhonenumberValidator(RegexValidator):
+    regex = r'^\d{3}-\d{3}-\d{4}$'
+    message = 'Invalid phone number'
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    phonenumber = models.CharField(max_length=12)
+    email = models.CharField(max_length=50, validators=[EmailValidator()])
+    phonenumber = models.CharField(max_length=12, validators=[PhonenumberValidator()])
 
     ADMIN = "ADM"
     REGULAR = "REG"
